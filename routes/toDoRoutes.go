8337e99ad9902ed8d5d22805347258f0f9009cd4
Erickype <erickype@hotmail.com>
2022-09-01 14:15:08 -0500

    Functions declarations of web methods

---
+++ routes/toDoRoutes.go
@@ -0,0 +1,16 @@
+package routes
+
+import "fmt"
+
+func getHandler() {
+	fmt.Println("Get")
+}
+func postHandler() {
+	fmt.Println("Post")
+}
+func updateHandler() {
+	fmt.Println("Update")
+}
+func deleteHandler() {
+	fmt.Println("Delete")
+}
